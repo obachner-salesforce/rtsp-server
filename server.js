e291e7c4b0ad74cbb44fa5729a57ea6c7fe3b9c0
Ofer Bachner <obachner@salesforce.com>
2023-12-17 16:28:18 +0200

    update link to rtsp

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Stream = require('node-rtsp-stream-jsmpeg')
 
 const options = {
   name: 'streamName',
-  url: 'rtp://127.0.0.1:9988',
+  url: 'rtsp://rtspstream:b7eb5fd20c01fd0d2dac045713f4b26f@zephyr.rtsp.stream/movie',
   wsPort: 3333
 }
 
